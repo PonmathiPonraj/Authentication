bee8104463c875ebb5c0574e3d09b929658e5368
ponmathi <ponmathittl@gmail.com>
2023-09-21 11:54:11 +0530

    update old password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,8 +1,10 @@
 import { useState, useRef, useContext } from 'react';
 import classes from './AuthForm.module.css';
 import AuthContext from '../../store/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 const AuthForm = () => {
+  const history=useHistory();
   const [isLogin, setIsLogin] = useState(true);
    const [isLoading,setIsLoading]=useState(false);
   const emailInputRef = useRef();
@@ -55,6 +57,7 @@ const AuthForm = () => {
         }
       }).then((data)=>{
         authCtx.login(data.idToken);
+        history.replace('/');
       }).catch(err=>{
         alert(err.Message);
       })
