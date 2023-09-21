45a607091687449e5075bb904bb8c87b7eb94fe8
ponmathi <ponmathittl@gmail.com>
2023-09-21 12:09:16 +0530

    store token in local storage

--- src/store/AuthContext.js
+++ src/store/AuthContext.js
@@ -1,4 +1,4 @@
-import  React,{useState} from 'react';
+import  React, { useState } from 'react';
     
 const AuthContext=React.createContext ({
     token:'',
@@ -8,14 +8,18 @@ const AuthContext=React.createContext ({
 })
 
 export const AuthContextProvider = (props) => {
-   const[token,setToken]=useState(null);
-   const userIsLoggedIn=!!token;
+    const intialToken=localStorage.getItem('token')
+    const[token,setToken]=useState(intialToken);
+    const userIsLoggedIn=!!token;
+
     const loggedInHandler=(token)=>{
-        setToken(token)
+        setToken(token);
+        localStorage.setItem('token',token);
     }
 
     const loggedOutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     }
 
     const contextValue = {
