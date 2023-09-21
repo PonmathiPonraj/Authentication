e98a76d13c2403c9587470737139d77ce3922f61
ponmathi <ponmathittl@gmail.com>
2023-09-21 10:59:49 +0530

    login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,55 +1,60 @@
 import { useState, useRef } from 'react';
 import classes from './AuthForm.module.css';
-
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading,setIsLoading]=useState(false);
-
+   const [isLoading,setIsLoading]=useState(false);
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
-
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
-
   const submitHandler = (event) => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    setIsLoading(true);
+     setIsLoading(true);
+     let url;
+
 
     if (isLogin) {
       // Handle login logic here
-    } 
-    else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=YOUR_API_KEY', {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((res) => {
-          setIsLoading(false);
-          if (res.ok) {
-            // Account created successfully, handle accordingly
-          } 
-          else {
-            return res.json().then((data) => {
-              let errorMessage = 'Authentication error';
-              if (data && data.error && data.error.message) {
-                errorMessage = data.error.message;
-              }
-              alert(errorMessage);
-            });
-          }
-        });
+
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDCn6Aksa3Ivlf_0FfXgHQb5cilhsxb0ZE'
+    } else {
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCn6Aksa3Ivlf_0FfXgHQb5cilhsxb0ZE'
     }
+    fetch(url , {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          // Account created successfully, handle accordingly
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication error';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+
+            throw new Error(errorMessage)
+          });
+        }
+      }).then((data)=>{
+        console.log(data);
+      }).catch(err=>{
+        alert(err.Message);
+      })
   };
 
   return (
@@ -65,7 +70,7 @@ const AuthForm = () => {
           <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-
+        
           {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}
           </button>}
           {isLoading && <p>Sending Request.....</p>}
@@ -83,3 +88,5 @@ const AuthForm = () => {
 };
 
 export default AuthForm;
+
+// https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCn6Aksa3Ivlf_0FfXgHQb5cilhsxb0ZE
